9b329ae20747c1101b70ea24c9bde5e8c0202fc4
mohammadAminMirzayi <76836263+mohammadAminMirzayi@users.noreply.github.com>
2021-02-13 14:47:19 +0330

    خدایا مرسیی

--- javaScript/app-.js
+++ javaScript/app-.js
@@ -1,89 +1,88 @@
 // if scroll change header backgrond color and size backgroundcolor white
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
-    
-    if(window.pageYOffset > 0){
+
+    if (window.pageYOffset > 0) {
         header.classList.add('sticky')
         arrowUp.classList.add('show-arrowUp')
-    }else{
+    } else {
         header.classList.remove('sticky')
-        
+
     }
 })
 // if scroll change header backgrond color and size backgroundcolor yellow
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
-    
-    if(window.pageYOffset > 690){
+
+    if (window.pageYOffset > 690) {
         header.classList.add('sticky-3')
-    }else
-    {
+    } else {
         header.classList.remove('sticky-3')
         arrowUp.classList.remove("show-arrowUp")
-       
+
     }
-        
-    
+
+
 })
 // if scroll change header backgrond color and size backgroundcolor green
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     const header = document.querySelector('.header')
-    
-    if(window.pageYOffset > 990){
+
+    if (window.pageYOffset > 990) {
         header.classList.add('sticky-')
-        
-    }else{
+
+    } else {
         header.classList.remove('sticky-')
     }
 })
- // arrowup button
+// arrowup button
 const arrowUp = document.querySelector('.Up')
 
 // if click the button (arrowup) scrollto 0 0 of index
-arrowUp.addEventListener('click', (e)=>{
+arrowUp.addEventListener("click", (e) => {
     e.preventDefault()
     window.scrollTo(0, 0)
 })
 
 
-
 // show mobile menu and hide if click button
 const btn = document.querySelector("#btn"),
-headerMenu = document.querySelector('.header-menu')
+    headerMenu = document.querySelector('.header-menu')
 let nav = document.querySelectorAll(".nav")
 
 btn.addEventListener("click", () => {
     // e.preventDefault()
-    if(headerMenu.className === "header-menu"){
+    if (headerMenu.className === "header-menu") {
         headerMenu.classList.add("show")
-    }else{
+    } else {
         headerMenu.classList.remove("show")
     }
-    
-    if(headerMenu.className === "show"){
+
+    if (headerMenu.className === "show") {
         headerMenuItem = document.querySelectorAll('.header-menu-item').style.display = "block";
-      
+
     }
 })
 
 const trint = document.querySelector('#trint'),
- tape = document.querySelector('#tape'),
- truck = document.querySelector('#truck'),
- edit = document.querySelector('#edit'),
- mainSection = document.querySelector(".phone"),
- ptTrint = document.querySelector(".trint-p")
+    tape = document.querySelector('#tape'),
+    truck = document.querySelector('#truck'),
+    edit = document.querySelector('#edit'),
+    mainSection = document.querySelector(".phone"),
+    ptTrint = document.querySelector(".trint-p")
 
- // icon section listener click
+// icon section listener click
 
- trint.addEventListener('click', ()=>{
-   
- })
- truck.addEventListener('click', ()=>{
-   
- })
- edit.addEventListener('click', ()=>{
-   
- })
- tape.addEventListener('click', ()=>{
-   
- })
+trint.addEventListener('click', () => {
+    //TODO
+    //FIXME
+});
+truck.addEventListener('click', () => {
+
+});
+edit.addEventListener('click', () => {
+
+});
+tape.addEventListener('click', () => {
+
+})
